597f04e339425571190c4c830ebb82c49e24ed58
Tui Popenoe <tuipopenoe@gmail.com>
2014-06-30 17:26:06 -0500

    createGrid from File

--- createGrid.py
+++ createGrid.py
@@ -1,16 +1,32 @@
 # Tui Popenoe
 # createGrid 
-"""Python utility to create a grid of the given character, X * Y"""
+"""Python utility to create a grid of the given character, X * Y
+    createGrid -e 10 10 x to create a 10x10 grid of x character.
+    createGrid -f to crate a grid from an input file"""
 
 import sys
 
-def createGrid(x, y, char='0'):
+def createEmptyGrid(x, y, char='0'):
     grid = [[char for i in xrange(x)] for j in xrange(y)]
     return grid
 
-def main():
-    createGrid(sys.argv[1], sys.argv[2], sys.argv[3])
+def creatGridFromFile(filename):
+    f = open(filename, 'r')
+    grid = list()
+    for line in list:
+        grid.append([])
+        for char in line:
+            grid[line].append(char):
 
+    f.close()
+
+    return grid
+
+def main():
+    if sys.argv[1] == '-e':
+        createEmptyGrid(sys.argv[2,3,4])
+    elif sys.argv[1] == '-f':
+        createGridFromFile(sys.argv[2])
 
 if __name__=='__main__':
     main()
